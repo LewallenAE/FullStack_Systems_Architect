9b7dd57c2e0a95af74440a97cb0f079d886a2548
Anthony Lewallen <lewallenae@gmail.com>
2025-07-13 15:16:34 -0700

    More JS Updates
    More JS Updates completing the Intro

--- OneDrive/Desktop/AnthonyLewallenDev/FrontEndMasters/01Beginner/Intro To Web Development/JavaScript/10 Objects.js
+++ OneDrive/Desktop/AnthonyLewallenDev/FrontEndMasters/01Beginner/Intro To Web Development/JavaScript/10 Objects.js
@@ -34,3 +34,59 @@ if (angie.age === "20 - 42") {
 } else {
     console.log("Nothing to see here.");
 }
+
+// Objects can contain other objects
+
+
+const johnnyCash = {
+    name: "Jonny Cash",
+    age: 71,
+    songs: ["Ring of Fire", "Hurt", "Folsom Prison Blues"],
+    playGuitar() {
+        console.log("Plays guitar with a southerny twang!");
+    }
+}
+
+johnnyCash.playGuitar();
+
+const person = {
+    Occupation: "Coding-warlock",
+    Experience: "Since the beginning of the internet",
+
+    /*
+    startsCoding: () => {
+        return "Anthony starts coding at the speed of whatever!"
+    },*/
+
+    startsCoding:() => "Anthony starts coding at the speed of whatever!",
+
+    // The commented code below would have to be called outside of the other template literal code as   person.startsCoding().
+    /* startsCoding: () => {
+        console.log("Anthony starts coding at the speed of whatever!");
+    } */   
+
+    personalInfo:{
+        name: "Anthony",
+        age: 39,
+    address: {
+            street: "12345 E. Coder Street",
+            city: "Code-city",
+            state: "Code-state",
+            zip: "12345",
+        }
+    }
+    };
+
+// Using string concatenation
+console.log("Person one's name: " + person.personalInfo.name + "\nPerson one's city: " + person.personalInfo.address.city); // this can get messy in larger scaled code systtems.
+
+// using template literals which is preferred and much cleaner
+console.log(`Person one's name: ${person.personalInfo.name},
+Person one's city: ${person.personalInfo.address.city},
+Occupation: ${person.Occupation},
+Experience: ${person.Experience}!,
+${person.startsCoding()}`);
+
+
+// person.StartsCoding();
+
